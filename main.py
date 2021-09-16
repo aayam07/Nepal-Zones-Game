72b6202b85b52f1396135e484809f51ebc139cc6
Aayam Adhikari <thisisleo7@gmail.com>
2021-09-16 22:06:28 +0545

    Cleaned up the code

--- main.py
+++ main.py
@@ -9,19 +9,6 @@ map_of_nepal = "./map_of_nepal_outline.gif"
 screen.addshape(map_of_nepal)
 turtle.shape(map_of_nepal)  # to display map of nepal onto our screen
 
-# This part is to get the screen coordinates of all the zones
-# for our .csv file.
-
-# def get_screen_cor(x, y):
-#     with open("./screen_cordinates.txt", mode="a") as file:
-#         file.write(f"{x, y}")
-#
-# # Event listeners
-# # when screen is clicked by mouse, the clicked position's x,y is passed as
-# # argument to the function
-# screen.onscreenclick(fun=get_screen_cor)
-# turtle.mainloop()
-
 
 def get_missing_zones(map_zones, user_zones):
     missed_zones = []
@@ -36,7 +23,6 @@ def get_missing_zones(map_zones, user_zones):
 # Converting .csv to dataframe using pandas
 data = pandas.read_csv("./zones_cordinates.csv")
 all_zones = data["zones"].to_list()  # converting zones series to a list of all the zones
-# print(all_zones)
 
 
 score = 0
